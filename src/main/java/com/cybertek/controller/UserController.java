877b2664e6fc8fde9650d2b668cbacffddba770b
GeorgeLondon1 <radac.george@yahoo.com>
2021-01-02 21:41:56 +0200

    JD - Ticketing Project - ORM

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -25,16 +25,16 @@ public class UserController {
 
         model.addAttribute("user",new UserDTO());
         model.addAttribute("roles",roleService.listAllRoles());
-        //model.addAttribute("users",userService.findAll());
+        model.addAttribute("users",userService.listAllUsers());
 
         return "/user/create";
     }
 
-//    @PostMapping("/create")
-//    public String insertUser(UserDTO user,Model model){
-//        userService.save(user);
-//        return "redirect:/user/create";
-//    }
+    @PostMapping("/create")
+    public String insertUser(UserDTO user,Model model){
+        userService.save(user);
+        return "redirect:/user/create";
+    }
 //
 //    @GetMapping("/update/{username}")
 //    public String editUser(@PathVariable("username") String username,Model model){
--- src/main/java/com/cybertek/implementation/UserServiceImpl.java
+++ src/main/java/com/cybertek/implementation/UserServiceImpl.java
@@ -1,16 +1,35 @@
 package com.cybertek.implementation;
 
 import com.cybertek.dto.UserDTO;
+import com.cybertek.entity.User;
+import com.cybertek.mapper.UserMapper;
+import com.cybertek.repository.UserRepository;
 import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    UserRepository userRepository;
+    UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
     @Override
     public List<UserDTO> listAllUsers() {
-        return null;
+        List<User> list = userRepository.findAll(Sort.by("firstName"));
+
+        return list.stream().map(obj ->{
+            return userMapper.convertToDto(obj);
+        }).collect(Collectors.toList());
     }
 
     @Override
@@ -20,6 +39,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void save(UserDTO dto) {
+        User obj = userMapper.convertToEntity(dto);
+        userRepository.save(obj);
 
     }
 
