c76205de5f14e5908f5e386fc9106f84e9ccc232
GeorgeLondon1 <radac.george@yahoo.com>
2021-01-06 03:47:51 +0200

    JD - Ticketing Project - ORM -> Project - ProjectDTO - Converter,Service,Impl

--- src/main/java/com/cybertek/converter/ProjectDtoConverter.java
+++ src/main/java/com/cybertek/converter/ProjectDtoConverter.java
@@ -1,6 +1,7 @@
 package com.cybertek.converter;
 
 import com.cybertek.dto.ProjectDTO;
+import com.cybertek.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
@@ -8,13 +9,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationPropertiesBinding
-public class ProjectDtoConverter {
+public class ProjectDtoConverter extends Converter<String,ProjectDTO>{
 
-//    @Autowired
-//    ProjectService projectService;
-//
-//    @Override
-//    public ProjectDTO convert(String source) {
-//        return projectService.findById(source)   ;
-//    }
+    @Autowired
+    ProjectService projectService;
+
+    @Override
+    public ProjectDTO convert(String source) {
+        return projectService.getByProjectCode(source);
+    }
 }
